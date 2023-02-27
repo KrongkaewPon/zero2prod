63a8594ef3d3a438b8cbdd35a2d31c8dc31e9595
Krongkaew Ponboobpha <krongkaew@enres.co>
2023-02-27 16:55:44 +0700

    log

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
+use env_logger::Env;
 use sqlx::PgPool;
-use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
-// #[tokio::main]
-// async fn main() -> std::io::Result<()> {
-//     let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
-//     run(listener)?.await
-// }
-
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    // `init` does call `set_logger`, so this is all we need to do.
+    // We are falling back to printing all logs at info-level or above
+    // if the RUST_LOG environment variable has not been set.
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration."); // We have removed the hard-coded `8000` - it's now coming from our settings!
     let connection = PgPool::connect(&configuration.database.connection_string())
