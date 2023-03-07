0d378ee69269636744c798005269bbb5b3dbfce3
Krongkaew Ponboobpha <krongkaew@enres.co>
2023-03-07 15:08:47 +0700

    section7 Reject Invalid Subscribers #2

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,5 @@
-use secrecy::ExposeSecret;
-use sqlx::postgres::PgPoolOptions;
-use sqlx::PgPool;
-use std::net::TcpListener;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 use zero2prod::configuration::get_configuration;
-use zero2prod::startup::run;
+use zero2prod::startup::Application;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
@@ -13,15 +7,8 @@ async fn main() -> std::io::Result<()> {
     let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
 
-    // Panic if we can't read configuration
-    let configuration = get_configuration().expect("Failed to read configuration."); // We have removed the hard-coded `8000` - it's now coming from our settings!
-    let connection_pool = PgPoolOptions::new().connect_lazy_with(configuration.database.with_db());
-
-    let address = format!(
-        "{}:{}",
-        configuration.application.host, configuration.application.port
-    );
-    let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool)?.await?;
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let application = Application::build(configuration).await?;
+    application.run_until_stopped().await?;
     Ok(())
 }
