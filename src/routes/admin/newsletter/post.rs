310adc906078b8df7b2e9115bf29fcbd4ab01b53
Krongkaew Ponboobpha <krongkaew@enres.co>
2023-03-31 14:20:14 +0700

    section 11 p415 Transaction Isolation

--- src/routes/admin/newsletter/post.rs
+++ src/routes/admin/newsletter/post.rs
@@ -10,6 +10,7 @@ use crate::email_client::EmailClient;
 use crate::idempotency::get_saved_response;
 use crate::idempotency::save_response;
 use crate::idempotency::IdempotencyKey;
+use crate::idempotency::{try_processing, NextAction};
 use crate::utils::{e400, e500, see_other};
 
 #[derive(serde::Deserialize)]
@@ -83,13 +84,17 @@ pub async fn publish_newsletter(
     } = form.0;
 
     let idempotency_key: IdempotencyKey = idempotency_key.try_into().map_err(e400)?;
-    if let Some(saved_response) = get_saved_response(&pool, &idempotency_key, *user_id)
+
+    let transaction = match try_processing(&pool, &idempotency_key, *user_id)
         .await
         .map_err(e500)?
     {
-        FlashMessage::info("The newsletter issue has been published!").send();
-        return Ok(saved_response); // Return early if we have a saved response in the database
-    }
+        NextAction::StartProcessing(t) => t,
+        NextAction::ReturnSavedResponse(saved_response) => {
+            success_message().send();
+            return Ok(saved_response);
+        }
+    };
 
     let subscribers = get_confirmed_subscribers(&pool).await.map_err(e500)?;
     for subscriber in subscribers {
@@ -107,6 +112,7 @@ pub async fn publish_newsletter(
                 tracing::warn!(
                 // We record the error chain as a structured field // on the log record.
                 error.cause_chain = ?error,
+                error.message = %error,
                 // Using `\` to split a long string literal over // two lines, without creating a `\n` character.
                 "Skipping a confirmed subscriber. \
                 Their stored contact details are invalid",
@@ -115,13 +121,16 @@ pub async fn publish_newsletter(
         }
     }
 
-    FlashMessage::info("The newsletter issue has been published!").send();
+    success_message().send();
     let response = see_other("/admin/newsletters");
-    let response = save_response(&pool, &idempotency_key, *user_id, response)
+    let response = save_response(transaction, &idempotency_key, *user_id, response)
         .await
         .map_err(e500)?;
-
     Ok(response)
+}
+
+fn success_message() -> FlashMessage {
+    FlashMessage::info("The newsletter issue has been published!")
 }
 
 struct ConfirmedSubscriber {
